88c133e4ab83eeb8b4b8ed474ae5b9de5a5b8939
MiddleTime <nadoroge@gmail.com>
2019-03-18 14:03:32 +0200

    fix bl

---
+++ Blog1/Engine/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace TestProject\Engine;
+
+final class Config
+{
+    const
+    DB_HOST = 'localhost',
+    DB_NAME = 'test3000',
+    DB_USR = 'root',
+    DB_PWD = '',
+
+    // Title of the site
+    SITE_NAME = 'My Simple Blog!';
+}
